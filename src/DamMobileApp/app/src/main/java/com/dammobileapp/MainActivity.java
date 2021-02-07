a27338a191c822f35fb020febb321b8d6e83b805
giacomoaccursi <giacomo.accursi@gmail.com>
2021-02-07 16:27:04 +0100

    added bt functions

--- src/DamMobileApp/app/src/main/java/com/dammobileapp/MainActivity.java
+++ src/DamMobileApp/app/src/main/java/com/dammobileapp/MainActivity.java
@@ -1,25 +1,45 @@
 package com.dammobileapp;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dammobileapp.netutils.Http;
+import com.dammobileapp.netutils.C;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.Random;
+import java.nio.BufferUnderflowException;
+
+import unibo.btlib.BluetoothChannel;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothChannel btChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(btAdapter != null && !btAdapter.isEnabled()){
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), C.bluetooth.ENABLE_BT_REQUEST);
+        }
+
 
         initUI();
     }
@@ -27,42 +47,74 @@ public class MainActivity extends AppCompatActivity {
     private void initUI() {
         tryHttpGet();
 
-        /*findViewById(R.id.postBtn).setOnClickListener(v -> {
-            try {
-                tryHttpPost();
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String toSend = "20";
+                switch (checkedId){
+                    case R.id.button20:
+                        toSend = "20";
+                    case R.id.button40:
+                        toSend = "40";
+                    case R.id.button60:
+                        toSend = "60";
+                    case R.id.button80:
+                        toSend = "80";
+                    case R.id.button100:
+                        toSend = "100";
+                }
+                btChannel.sendMessage(toSend);
+            }
+        });
+
+        Button mode = (Button)findViewById(R.id.modeButton);
+        mode.setOnClickListener(v -> {
+           String modeText = (String) ((Button) findViewById(R.id.modeButton)).getText();
+           try {
+                mode.setText(modeText == "MANUAL"? "AUTOMATIC" : "MANUAL");
+                tryHttpPost(modeText == "MANUAL"? "AUTOMATIC" : "MANUAL");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        });*/
+
+        });
     }
 
     private void tryHttpGet(){
         final String url = "http://dummy.restapiexample.com/api/v1/employee/1";
-
         Http.get(url, response -> {
-            if(response.code() == HttpURLConnection.HTTP_OK){
+            if(response.code() == HttpURLConnection.HTTP_OK) {
                 try {
-                    ((TextView)findViewById(R.id.level)).setText(response.contentAsJson().getString("level"));
-                    ((TextView)findViewById(R.id.state)).setText(response.contentAsJson().getString("state"));
-                    ((TextView)findViewById(R.id.opening)).setText(response.contentAsJson().getString("opening"));
+                    JSONObject content = response.contentAsJson();
+                   // ((TextView) findViewById(R.id.level)).setText(content.getString("level"));
+                    ((TextView) findViewById(R.id.state)).setText(content.getString("status"));
+                  //  ((TextView) findViewById(R.id.opening)).setText(content.getString("opening"));
 
-                    if(response.contentAsJson().getString("state") == "alarm"){
-                        findViewById(R.id.state).setEnabled(true);
+                    if (content.has("status")) {
+                        if (content.getString("status").equals("success")) {
+                            findViewById(R.id.modeButton).setEnabled(true);
+                            findViewById(R.id.radioGroup).setVisibility(View.VISIBLE);
+                        } else {
+                            findViewById(R.id.modeButton).setEnabled(false);
+                            findViewById(R.id.radioGroup).setVisibility(View.GONE);
+                        }
                     }
-
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
+                    } catch(IOException | JSONException e){
+                        e.printStackTrace();
+                    }
             }
         });
-    }
 
+}
 
-    private void tryHttpPost() throws JSONException {
+    private void tryHttpPost(String mode) throws JSONException {
 
         final String url = "http://dummy.restapiexample.com/api/v1/employee/1";
         final String content = new JSONObject()
-                .put("mode","manual").toString();
+                .put("mode",mode).toString();
 
         Http.post(url, content.getBytes(), response -> {
             if(response.code() == HttpURLConnection.HTTP_OK){
--- src/DamMobileApp/app/src/main/java/com/dammobileapp/netutils/Http.java
+++ src/DamMobileApp/app/src/main/java/com/dammobileapp/netutils/Http.java
@@ -1,6 +1,9 @@
 package com.dammobileapp.netutils;
 
 import android.os.AsyncTask;
+
+import org.json.JSONException;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,13 +12,14 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Http {
 
     public interface Listener{
-        void onHttpResponseAvailable(final HttpResponse response);
+        void onHttpResponseAvailable(final HttpResponse response) throws IOException, JSONException;
     }
 
     public static void get(final String url, final Listener listener){
         new AsyncTask<Void, Void, HttpResponse>() {
             @Override
             protected HttpResponse doInBackground(Void... voids) {
+                //while (true){
                 final HttpURLConnection connection;
                 try {
                     connection = (HttpURLConnection) new URL(url).openConnection();
@@ -26,14 +30,21 @@ public class Http {
                 } catch (IOException e) {
                     return null;
                 }
+                //}
             }
 
             @Override
             protected void onPostExecute(HttpResponse response) {
-                listener.onHttpResponseAvailable(response);
+                try {
+                    listener.onHttpResponseAvailable(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-       // }.execute();
+       //}.execute();
     }
 
     public static void post(final String url, final byte[] payload, final Listener listener){
@@ -55,7 +66,13 @@ public class Http {
 
             @Override
             protected void onPostExecute(HttpResponse response) {
-                listener.onHttpResponseAvailable(response);
+                try {
+                    listener.onHttpResponseAvailable(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         //}.execute();
