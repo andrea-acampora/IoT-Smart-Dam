623b4d5e4994151686ad983b92047c9d5b983d13
giacomoaccursi <giacomo.accursi@gmail.com>
2021-02-10 17:37:46 +0100

    small change in json read and object visualization

--- src/dam_dashboard/data.js
+++ src/dam_dashboard/data.js
@@ -1,47 +1,57 @@
 $(document).ready(function () {
-	
-	let levels = []; 
-	let x = 0 ; 
-	let y = 0; 
+
+	let levels = [];
+	let x = 0;
+	let y = 0;
 	const levels_length = 6
-	let requestLoop = setInterval(function(){
+	let requestLoop = setInterval(function () {
 		$.getJSON('http://042c464fe0ae.ngrok.io/api/data', function (data) {
-			 console.log(data);
-			let state = data.state; 
-			let level = data.value; 
-			let mode = data.mode; 
-			let opening = data.opening; 
-			let timestamp = data.timeStamp; 
-
-			levels.push(new Array(level, timestamp));
-			if(levels.length > levels_length){
-				levels.shift(); 
-			}
-
-			console.log(levels); 
-
-			let result = ""; 
-			for(let i = 0; i < levels.length-1; i++){
-				result += `<div>`+ levels[i][0] + ` - `+ levels[i][1]+`</div>`; 
-			}
-			$(".prev-levels").empty();
-			$(".prev-levels").append(result);
-
-
-			let current = `<div>`+ levels[levels.length-1][0] + `</div>`; 
 			
+			let state = data.state;
+			let timestamp;
+			let level;
+			let mode;
+			let opening;
+			
+			$(".mode").empty();
+			$(".state").empty();
+			$(".opening").empty();
+			$(".prev-levels").empty();
 			$(".current-level").empty();
-			$(".current-level").append(current);
 
-			$(".state").empty(); 
-			$(".mode").empty(); 
-			$(".opening").empty(); 
+			$(".state").text(state);
 
-			$(".state").text(state); 
-			$(".mode").text(mode); 
-			$(".opening").text(opening); 
+			if ("level" in data) {
+				timestamp = data.timeStamp;
+				level = data.value;
+				levels.push(new Array(level, timestamp));
+				if (levels.length > levels_length) {
+					levels.shift();
+				}
+				let result = "";
+				for (let i = 0; i < levels.length - 1; i++) {
+					result += `<div>` + levels[i][0] + ` - ` + levels[i][1] + `</div>`;
+				}
+				
+				$(".prev-levels").append(result);
+				let current = `<div>` + levels[levels.length - 1][0] + `</div>`;
+	
+				$(".current-level").append(current);
+			}else{
+				levels = []; 
+			}
+
+			if("mode" in data) {
+				mode = data.mode;
+				$(".mode").text(mode);
+			}
+			
+			if("opening" in data) {
+				opening = data.opening;
+				$(".opening").text(opening);
+			}
 		});
-	}, 5000); 
+	}, 5000);
 
-	   
+
 });
